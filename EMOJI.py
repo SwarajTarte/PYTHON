f78d52f72fcc8a145199d00652e3c506fd069fe1
Swaraj Tarte <swaraj.codes@gmail.com>
2024-12-29 20:37:37 +0530

    1st

---
+++ EMOJI.py
@@ -0,0 +1,8 @@
+# This code will use Emoji module to print emojis in output.
+# Note: You need to install emoji module first using pip install emoji.
+
+import emoji
+#This command (:snake:) will print a snake emoji.
+print(emoji.emojize('There is a :snake: in my boot!'))
+
+#You can change :snake: to any other emoji name to print that emoji.
