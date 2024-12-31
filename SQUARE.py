5c398a2e6bf470a2650dcb7772d15c222a04d884
Swaraj Tarte <swaraj.codes@gmail.com>
2024-12-31 12:59:38 +0530

    8th

---
+++ SQUARE.py
@@ -0,0 +1,6 @@
+#This code will provide the square of the entered no.
+
+a = float(input("Enter no.: "))
+
+print("Square of the entered no. is: ", a**2)
+#you cant write a^2 as it is incorrect syntax. You have to write a**2
