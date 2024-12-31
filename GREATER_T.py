3e45a95255328472a01c60cef237dc50eb692f76
Swaraj Tarte <swaraj.codes@gmail.com>
2024-12-31 12:58:40 +0530

    6th

---
+++ GREATER_T.py
@@ -0,0 +1,6 @@
+#This code will tell which number is greater among the two numbers
+
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+
+print("a is greater than b is", a>b)
