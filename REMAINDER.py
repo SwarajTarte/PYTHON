73403505fed5a55d5aac3a55dbc1117894faf5d9
Swaraj Tarte <swaraj.codes@gmail.com>
2024-12-31 12:57:57 +0530

    5th

---
+++ REMAINDER.py
@@ -0,0 +1,5 @@
+#This code will print the remainder of divisor divided by dividend enterd by you.
+
+a = float(input("Enter no. Dividend: "))
+b = float(input("Enter no. Divisor: "))
+print("Remainder of", a, "divided by", b, "is", a%b)
